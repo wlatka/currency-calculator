49013d51abf29a2975b87d260bd514a802fd6887
Wojciech Latka <w.latka@itprotech.pl>
2021-06-10 06:27:09 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
-console.log("Hello to all developers!");
+{
+    const welcome = () => {
+        console.log("Hello to all developers!");
+    };
 
-let formElement = document.querySelector(".js-form");
-let euroElement = document.querySelector(".js-euro");
-let plnElement = document.querySelector(".js-pln");
-let usdElement = document.querySelector(".js-usd");
-let gbpElement = document.querySelector(".js-gbp");
+    const amountEuro = () => {
+        const euroElement = document.querySelector(".js-euro");
+        const euro = euroElement.value;
+    };
 
-formElement.addEventListener("input", () => {
-    let euro = euroElement.value;
-    let pln = 4.5577 * euro;
-    let usd = 0.83 * euro;
-    let gbp = 0.87 * euro;
+    const resultText = (euro) => {
+        const plnElement = document.querySelector(".js-pln");
+        const usdElement = document.querySelector(".js-usd");
+        const gbpElement = document.querySelector(".js-gbp");
 
-    plnElement.innerText = `${pln.toFixed(2)} zł`;
-    usdElement.innerText = `${usd.toFixed(2)} $`;
-    gbpElement.innerText = `${gbp.toFixed(2)} £`;
-});
+        const pln = 4.5577 * euro;
+        const usd = 0.83 * euro;
+        const gbp = 0.87 * euro;
+
+        plnElement.innerText = `${pln} zł`;
+        usdElement.innerText = `${usd} $`;
+        gbpElement.innerText = `${gbp} £`;
+       
+    };
+
+    const init = (pln) => {
+        const formElement = document.querySelector(".js-form");
+        formElement.addEventListener("input", resultText);
+
+    };
+    init();
+    welcome();
+
+};
