5c4935de9113c0ff2679b1b4712d7aa491b928a1
Wojciech Łatka <w.latka@itprotech.pl>
2021-08-02 21:05:41 +0200

    changed script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@
         console.log("Hello to all developers!");
     };
 
-    const amountEuro = () => {
+
+    const resultText = () => {
+
         const euroElement = document.querySelector(".js-euro");
         const euro = euroElement.value;
-    };
 
-    const resultText = (euro) => {
         const plnElement = document.querySelector(".js-pln");
         const usdElement = document.querySelector(".js-usd");
         const gbpElement = document.querySelector(".js-gbp");
@@ -17,18 +17,17 @@
         const usd = 0.83 * euro;
         const gbp = 0.87 * euro;
 
-        plnElement.innerText = `${pln} zł`;
-        usdElement.innerText = `${usd} $`;
-        gbpElement.innerText = `${gbp} £`;
-       
+        plnElement.innerText = `${pln.toFixed(2)} zł`;
+        usdElement.innerText = `${usd.toFixed(2)} $`;
+        gbpElement.innerText = `${gbp.toFixed(2)} £`;
+
     };
 
-    const init = (pln) => {
+    const init = () => {
         const formElement = document.querySelector(".js-form");
         formElement.addEventListener("input", resultText);
-
+        resultText();
+        welcome();
     };
     init();
-    welcome();
-
 };
